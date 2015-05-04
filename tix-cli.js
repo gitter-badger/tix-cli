4293fd4f17c58c09cd10ac3c35e61f1892a05d50
Cole Chamberlain <cole.chamberlain@gmail.com>
2015-05-04 13:59:56 -0700

    Update tix-cli.js

--- tix-cli.js
+++ tix-cli.js
@@ -681,7 +681,7 @@ function getPath(path) {
 
 /** Gets a readline (built-in) interface. */
 function createInterface() {
-  return require('readline').createInterface(process.stdin, process.stdout);
+  return require('readline').createInterface(process.stdin, process.stdout, terminal: false);
 }
 
 /** Ensures advanced CLI shell gets started in the cli directory so that npm dependencies will be loadable. */
