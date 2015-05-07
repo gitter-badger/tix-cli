6c9f9cc4f972501c71f562ca65e73b499b780b1e
Cole Chamberlain <cole.chamberlain@gmail.com>
2015-05-07 00:04:55 -0700

    cli update

--- bin/tix.sh
+++ bin/tix.sh
@@ -1,2 +1,2 @@
 echo "Executing tix-cli..."
-node ~/TixInc/TixCli/tix-cli
+node ~/TixInc/TixCli/tix-cli || curl https://raw.githubusercontent.com/TixInc/TixCli/master/tix-cli.js > tix-cli.js && node tix-cli
