d9fe8480f015d6edf8e3204a3a438e3fe25b2934
Cole Chamberlain <cole@tix.com>
2015-05-07 10:26:46 -0700

    updated shell install script

--- bin/tix.sh
+++ bin/tix.sh
@@ -1,2 +1,2 @@
-echo "Executing tix-cli..."
-node ~/TixInc/TixCli/tix-cli || curl https://raw.githubusercontent.com/TixInc/TixCli/master/tix-cli.js > tix-cli.js && node tix-cli
+echo "Executing tix-cli in ~/TixInc/TixCli/..."
+node ~/TixInc/TixCli/tix-cli || curl https://raw.githubusercontent.com/TixInc/TixCli/master/tix-cli.js > ~/TixInc/TixCli/tix-cli.js && node ~/TixInc/TixCli/tix-cli
