35f8711fd7abe5f27caf7fe8fd0ba8ce53424a4c
Cole Chamberlain <cole.chamberlain@gmail.com>
2015-05-07 21:12:23 -0700

    Update msys2-sync-update.sh

--- bin/msys2-sync-update.sh
+++ bin/msys2-sync-update.sh
@@ -4,8 +4,8 @@ pacman --noconfirm -Sy
 echo "Package synchronization finished."
 
 echo "Updating core msys2 packages..."
-pacman -S --noconfirm --needed --force bash
-pacman -S --noconfirm --needed --force pacman
-pacman -S --noconfirm --needed --force pacman-mirrors
-pacman -S --noconfirm --needed --force msys2-runtime
+yes | pacman -S --needed bash pacman pacman-mirrors msys2-runtime
+#pacman -S --needed pacman
+#pacman -S --needed pacman-mirrors
+#pacman -S --needed msys2-runtime
 echo "Core packages updated..."
