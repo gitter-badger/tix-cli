d5a620d494ab0e90f737ec3694b9e5db95ad4006
Cole Chamberlain <cole@tix.com>
2015-05-07 19:37:47 -0700

    adding force....

--- bin/msys2-sync-update.sh
+++ bin/msys2-sync-update.sh
@@ -4,8 +4,8 @@ pacman --noconfirm -Sy
 echo "Package synchronization finished."
 
 echo "Updating core msys2 packages..."
-pacman -S --noconfirm --needed bash
-pacman -S --noconfirm --needed pacman
-pacman -S --noconfirm --needed pacman-mirrors
-pacman -S --noconfirm --needed msys2-runtime
+pacman -S --noconfirm --needed --force bash
+pacman -S --noconfirm --needed --force pacman
+pacman -S --noconfirm --needed --force pacman-mirrors
+pacman -S --noconfirm --needed --force msys2-runtime
 echo "Core packages updated..."
