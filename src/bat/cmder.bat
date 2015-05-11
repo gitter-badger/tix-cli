917c3ab58f866f19ca69598dcf16bfbd783dc5e4
Cole Chamberlain <cole.chamberlain@gmail.com>
2015-05-11 01:25:43 -0700

    pretty much working

--- src/bat/cmder.bat
+++ src/bat/cmder.bat
@@ -1,3 +1,4 @@
 @echo off
-set CMDER_ROOT=C:\Users\cchamberlain\local\cmder
+SET PATH=%HOMEPATH%\local\bin;%PATH%
+set CMDER_ROOT=%HOMEPATH%\local\cmder
 start %CMDER_ROOT%\vendor\conemu-maximus5\ConEmu.exe /icon "%CMDER_ROOT%\cmder.exe" /title Cmder /loadcfgfile "%CMDER_ROOT%\config\ConEmu.xml" /cmd cmd /k "%CMDER_ROOT%\vendor\init.bat cd %CD%"
