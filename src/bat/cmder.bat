0080a4b1287d5c54eec2d6e45270bbfa0e0ece89
Cole Chamberlain <cole@tix.com>
2015-05-11 12:54:27 -0700

    more

--- src/bat/cmder.bat
+++ src/bat/cmder.bat
@@ -1,6 +1,6 @@
 @echo off
 if "%~1"=="" set %LOCAL_ROOT%=%HOMEPATH%\local
+call %LOCAL_ROOT%\bin\source-paths.bat
 set CMDER_ROOT=%LOCAL_ROOT%\cmder
-call %CMDER_ROOT%\bin\source-paths.bat
 
 start %CMDER_ROOT%\vendor\conemu-maximus5\ConEmu.exe /icon "%CMDER_ROOT%\cmder.exe" /title Cmder /loadcfgfile "%CMDER_ROOT%\config\ConEmu.xml" /cmd cmd /k "%CMDER_ROOT%\vendor\init.bat cd %CD%"
