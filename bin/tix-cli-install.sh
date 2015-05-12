9d14d981bd95c5e41e308c3af0ce3a6e0cd360fc
Cole Chamberlain <cole@tix.com>
2015-05-11 19:40:14 -0700

    more

--- bin/tix-cli-install.sh
+++ bin/tix-cli-install.sh
@@ -1,1 +1,6 @@
-mkdir -p ~/tixinc/tix-cli && curl https://raw.githubusercontent.com/TixInc/tix-cli/master/tix-cli.js > ~/tixinc/tix-cli/tix-cli.js && node ~/tixinc/tix-cli/tix-cli.js
+echo "Ensuring directory"
+mkdir -p ~/tixinc/tix-cli
+echo "Downloading tix-cli"
+curl https://raw.githubusercontent.com/TixInc/tix-cli/master/tix-cli.js > ~/tixinc/tix-cli/tix-cli.js
+echo "Executing tix-cli"
+node ~/tixinc/tix-cli/tix-cli.js
