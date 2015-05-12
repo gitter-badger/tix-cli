081febebe417ae5515248c74c9eb85bc1da00a78
Cole Chamberlain <cole@tix.com>
2015-05-12 10:57:01 -0700

    updates to tix-cli-src

--- bin/push-all.bat
+++ bin/push-all.bat
@@ -1,3 +1,1 @@
-pushd ..\
-git add . && git commit -am "updates to tix-cli-src" && git push origin master
-popd
+pushd ..\ && git add . && git commit -am "updates to tix-cli-src" && git push origin master && popd
