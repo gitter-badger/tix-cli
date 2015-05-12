3266a1f97385cd25084aabcefa16080784965d2c
Cole Chamberlain <cole@tix.com>
2015-05-12 10:58:47 -0700

    updates to tix-cli-src

--- bin/push-reinstall.bat
+++ bin/push-reinstall.bat
@@ -1,6 +1,6 @@
 @echo off
 call %USERPROFILE%\local\config\variables.bat
 call push-all.bat
-call npm-publish.bat
-call cleanup.bat
+REM call npm-publish.bat
+REM call cleanup.bat
 call ..\src\cmd\download-install-src.cmd
