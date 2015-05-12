a6b1e4ada9b9ec9da217c647354cd28075e86caa
Cole Chamberlain <cole@tix.com>
2015-05-11 19:53:09 -0700

    more

--- bin/tix.sh
+++ bin/tix.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -ex
 echo "Executing tix-cli in ~/tixinc/tix-cli/..."
-node ~/tixinc/tix-cli/tix-cli.js || tix-cli-install.sh
+node ~/tixinc/tix-cli/tix-cli.js || mkdir -p ~/tixinc/tix-cli && curl https://raw.githubusercontent.com/TixInc/tix-cli/master/tix-cli.js > ~/tixinc/tix-cli/tix-cli.js && node ~/tixinc/tix-cli/tix-cli.js
