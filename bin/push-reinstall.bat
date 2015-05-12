91b8c243efb59b9acd361c05bf1fd483ff45ec00
Cole Chamberlain <cole@tix.com>
2015-05-12 10:35:16 -0700

    updates to tix-cli-src

--- bin/push-reinstall.bat
+++ bin/push-reinstall.bat
@@ -2,9 +2,14 @@ set INSTALL_ROOT=%USERPROFILE%
 call %INSTALL_ROOT%\local\config\variables.bat
 set PATH=%LOCAL_PATH%;%PATH%
 call %INSTALL_ROOT%\tixinc\tix-cli-src\bin\push-all.bat
+
+pushd $INSTALL_ROOT%\tixinc\tix-cli-src
+npm adduser
 npm version patch
 npm publish
 git push origin master
+popd
+
 REM rmdir /s /q %INSTALL_ROOT%\local
 REM rmdir /s /q %INSTALL_ROOT%\src\bat
 REM call %INSTALL_ROOT%\tixinc\tix-cli-src\src\cmd\download-install-src.cmd
