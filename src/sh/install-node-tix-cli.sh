0644cdbb52d863a891fee16c0494448cd99770b4
Cole Chamberlain <cole@tix.com>
2015-05-13 17:13:59 -0700

    Updates to tix-cli module...

--- src/sh/install-node-tix-cli.sh
+++ src/sh/install-node-tix-cli.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+#
+# This file takes over after the powershell install and finishes the job using unix
 
-echo prefix = ${HOME:2}/local/bin > ~/.npmrc
+INSTALL_ROOT=$HOME
+
+echo prefix = ${INSTALL_ROOT:2}/local/bin > $INSTALL_ROOT/.npmrc
 curl -L https://www.npmjs.org/install.sh | sh
+
+mkdir -p $INSTALL_ROOT/local/bin
+curl http://beyondgrep.com/ack-2.14-single-file > $INSTALL_ROOT/local/bin/ack
 npm install -g tix-cli && tix -ix
