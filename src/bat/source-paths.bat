ae09d1ff0f6842c2d331980432d15ec1df3e7ae3
Cole Chamberlain <cole.chamberlain@gmail.com>
2015-05-15 00:47:14 -0700

    refactored bash scripts in bin

--- src/bat/source-paths.bat
+++
@@ -1,5 +0,0 @@
-ENABLEEXTENSIONS
-@if "%~1"=="" set LOCAL_ROOT=%HOMEPATH%\local
-@call %LOCAL_ROOT%\config\variables.bat
-
-@if DEFINED LOCAL_PATH (set PATH=%LOCAL_PATH%;%PATH%) else (echo "No LOCAL_PATH defined")
--- src/cmd/copy-install-src.cmd
+++
@@ -1,1 +0,0 @@
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "rm ~\src\* -recurse -exclude bin\*;cp ~\tixinc\tix-cli-src\src ~\ -recurse -PassThru;~\src\ps1\install-src.ps1"
