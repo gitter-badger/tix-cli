1eb147ef01d6fb82654af3ab8ffcb46c9c7aaa94
Cole Chamberlain <cole@tix.com>
2015-05-18 12:37:54 -0700

    Updates to tix-cli module...

--- src/cmd/cmder.cmd
+++ src/cmd/cmder.cmd
@@ -1,4 +1,5 @@
 @echo off
-set LOCAL_ROOT=%USERPROFILE%\local && set CMDER_ROOT=%LOCAL_ROOT%\cmder
+set LOCAL_ROOT=%USERPROFILE%\local
+set CMDER_ROOT=%LOCAL_ROOT%\cmder
 call %LOCAL_ROOT%\config\variables.cmd
 IF [%1]==[] (start %CMDER_ROOT%\vendor\conemu-maximus5\ConEmu.exe -cur_console:n /icon "%CMDER_ROOT%\vendor\msysgit\etc\git.ico" /title Cmder /loadcfgfile "%CMDER_ROOT%\config\ConEmu.xml" /cmd cmd /c ""%CMDER_ROOT%\vendor\msysgit\bin\sh.exe" --login -i && exit") ELSE (start %CMDER_ROOT%\vendor\conemu-maximus5\ConEmu.exe -cur_console:n /icon "%CMDER_ROOT%\vendor\msysgit\etc\git.ico" /title Cmder /loadcfgfile "%CMDER_ROOT%\config\ConEmu.xml" /cmd cmd /c ""%CMDER_ROOT%\vendor\msysgit\bin\sh.exe" --login -i -- %*")
