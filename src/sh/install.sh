cee4ff738c3890023b8b21fa8398b3d5c3b60a8d
Cole Chamberlain <cole@tix.com>
2015-05-18 12:27:01 -0700

    Updates to tix-cli module...

--- src/sh/install.sh
+++ src/sh/install.sh
@@ -9,7 +9,7 @@ LOCAL_BIN_ROOT="${INSTALL_ROOT}/local/bin"
 echo "--Creating new ~/.npmrc--"
 echo prefix = ${LOCAL_BIN_ROOT:2} > $NPMRC_PATH
 
-echo "--Instalilng NPM--"
+echo "--Installing NPM--"
 curl -L https://www.npmjs.org/install.sh | sh
 
 echo "--Installing tix-cli with optional applications and extended mode--"
