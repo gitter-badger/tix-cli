fb4bc5e95503ec572f2b101d1b7819d9a6de4634
Cole Chamberlain <cole@tix.com>
2015-05-18 14:53:04 -0700

    accidentally overwrote file, undoing

--- src/ps1/extensions.ps1
+++ src/ps1/extensions.ps1
@@ -1,51 +1,156 @@
-@ECHO OFF
-SET LOCAL_ROOT=%USERPROFILE%\local
-SET CMDER_ROOT=%LOCAL_ROOT%\cmder
-CALL %LOCAL_ROOT%\config\variables.cmd
+param($INSTALL_ROOT=$HOME)
+#################################################
+# Reusable functions for powershell.            #
+#################################################
 
-SET cmd_switch=/s /c
-IF [%1]==[] GOTO RunNoParams
-
-SET is_command=false
-SET "arg_line="
-:ShiftParams
-IF "%1"=="" GOTO RunWithParams
-
-IF /I "%1"=="/k" (
-	ECHO Setting /k switch...
-	SET cmd_switch=/s /k
-	SHIFT
-	GOTO ShiftParams
-)
-
-IF /I "%1"=="/x" (
-	ECHO Setting /x switch-run as command
-	SET is_command=true
-	SHIFT
-	GOTO ShiftParams
-)
-
-SET arg_line=%arg_line% %1
-SHIFT
-GOTO ShiftParams
-
-:RunWithParams
-
-IF %is_command%==true GOTO RunWithCommand
-
-START %CMDER_ROOT%\vendor\conemu-maximus5\ConEmu.exe -cur_console:n /icon "%CMDER_ROOT%\vendor\msysgit\etc\git.ico" /title Cmder /loadcfgfile "%CMDER_ROOT%\config\ConEmu.xml" /cmd cmd %cmd_switch% ""%CMDER_ROOT%\vendor\msysgit\bin\sh.exe" --login -i -- "%arg_line%""
-
-GOTO EndScript
-
-:RunWithCommand
-
-START %CMDER_ROOT%\vendor\conemu-maximus5\ConEmu.exe -cur_console:n /icon "%CMDER_ROOT%\vendor\msysgit\etc\git.ico" /title Cmder /loadcfgfile "%CMDER_ROOT%\config\ConEmu.xml" /cmd cmd %cmd_switch% ""%CMDER_ROOT%\vendor\msysgit\bin\sh.exe" --login -i -c  "%arg_line%""
-
-GOTO EndScript
+$LOCAL_ROOT=Join-Path $INSTALL_ROOT local
+$7ZA_PATH=Join-Path $LOCAL_ROOT '7z\7za.exe'
+$LOCAL_BIN_ROOT=Join-Path $LOCAL_ROOT bin
+$CMDER_PATH=Join-Path $LOCAL_BIN_ROOT cmder.cmd
 
 
-:RunNoParams
+Filter Write-PipeTable {
+    Param( [switch]$PassThru )
+    Write-Host ($_ | Format-Table | Out-String)
+    If($PassThru) {
+      Write-Output $_
+    }
+}
 
-START %CMDER_ROOT%\vendor\conemu-maximus5\ConEmu.exe -cur_console:n /icon "%CMDER_ROOT%\vendor\msysgit\etc\git.ico" /title Cmder /loadcfgfile "%CMDER_ROOT%\config\ConEmu.xml" /cmd cmd /s /c ""%CMDER_ROOT%\vendor\msysgit\bin\sh.exe" --login -i"
+Filter Write-PipeList {
+    Param( [switch]$PassThru )
+    Write-Host ($_ | Format-List | Out-String)
+    if($PassThru) {
+      Write-Output $_
+    }
+}
 
-:EndScript
+Filter Ensure-Directory {
+  Param( [switch]$PassThru )
+  If(!(Test-Path -Path $_ -PathType Container)) {
+    New-Item -Path $_ -type Directory | Out-Null
+    If($PassThru) {
+      Write-Output $_
+    }
+  }
+}
+
+Function Expand-Zip ($zipPath, $destDir) {
+  $shell = New-Object -com shell.application
+  $zip = $shell.Namespace($zipPath)
+
+  $destDir|Ensure-Directory
+
+  ForEach($item in $zip.items()) {
+    $shell.Namespace($destDir).CopyHere($item, 0x10)
+  }
+}
+
+Function Execute($filePath, $arguments)
+{
+    If($arguments) {
+        Start-Process $filePath -ArgumentList $arguments -Wait -PassThru
+    }
+    Else {
+        Start-Process $filePath -Wait -PassThru
+    }
+}
+
+Function Execute-Sh($command) {
+    Execute $CMDER_PATH $command
+}
+
+Function Execute-7z($arguments) {
+    Write-Host "$7ZA_PATH $arguments"
+    Execute $7ZA_PATH $arguments
+}
+
+Function Expand-7z($filePath, $destDir) {
+  $destDir|Ensure-Directory
+  Execute-7z "x -aoa -o$destDir $filePath"
+}
+
+Function Execute-Ps1($filePath) {
+    Execute powershell.exe "-File=$filePath"
+}
+
+Function Install-Msi ($filePath, $arguments) {
+    Execute msiexec.exe "/i $filePath $arguments"
+}
+
+Function New-HardLink ($link, $target) {
+    Write-Host "Hard linking $target to $link"
+    Invoke-Expression "cmd /c mklink /H $link $target"
+}
+
+Function New-HardLinkIn ($dir, $target) {
+  $fileName = [System.IO.Path]::GetFileName($target)
+  $dir|Ensure-Directory
+  $link = Join-Path $dir $fileName
+  New-HardLink $link $target
+}
+
+Function New-SymLink ($link, $target) {
+    $baseCmd='cmd /c mklink'
+    If (Test-Path -PathType Container $target) {
+        $baseCmd+=' /d'
+    }
+    Write-Host "Symbolic linking $target to $link"
+    Invoke-Expression "$baseCmd $link $target"
+}
+
+Function New-SymLinkIn ($dir, $target) {
+  $fileName = [System.IO.Path]::GetFileName($target)
+  $dir|Ensure-Directory
+  $link = Join-Path $dir $fileName
+  New-SymLink $link $target
+}
+
+Function Remove-Path ($path) {
+    If (Test-Path $path) {
+      Remove-Item $path
+    }
+}
+
+# Downloads a file from url to directory
+Function Download-File($url, $filePath) {
+    If (!(Test-Path -Path $filePath -PathType Leaf)) {
+        Write-Host "Downloading $url to $filePath"
+        $webClient = New-Object System.Net.WebClient
+        $webClient.DownloadFile("$url", "$filePath")
+    }
+}
+
+Filter Download-Files {
+    Download-File $_.fileUrl $_.filePath
+}
+
+### This function currently is only useful for building a shortcut to cmder.exe, eventually will be more generic.
+Function Create-Shortcut($fileDir, $fileName) {
+  #$sa = new-object -c shell.application
+  #$ns = $sa.namespace($fileDir)
+  #$pn = parsename($fileName)
+  #$pn.invokeverb('taskbarpin')
+
+  $cmdName = 'cmder'
+  $cmdPath=Join-Path $fileDir $fileName
+  $iconPath = Join-Path $HOME local\cmder\vendor\msysgit\etc\git.ico
+  Write-Host "adding shortcut at $cmdPath"
+  $taskbarFolder = "$env:APPDATA\Microsoft\Internet Explorer\Quick Launch\User Pinned\Taskbar\"
+  $startMenuFolder = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\"
+  $desktopFolder = Join-Path $HOME Desktop
+  $objShell = New-Object -ComObject WScript.Shell
+  $objShortCut = $objShell.CreateShortcut("$startMenuFolder\$cmdName.lnk")
+  $objShortCut.IconLocation = "$iconPath"
+  $objShortCut.TargetPath = 'cmd'
+  $objShortCut.Arguments="/c ""$cmdPath"""
+  $objShortCut.Save()
+
+  $desktopShortCut = $objShell.CreateShortcut("$desktopFolder\$cmdName.lnk")
+  $desktopShortCut.IconLocation = "$iconPath"
+  $desktopShortCut.TargetPath = 'cmd'
+  $desktopShortCut.Arguments="/c ""$cmdPath"""
+  $desktopShortCut.Save()
+}
+
+Write-Host '--extensions.ps1 sourced--'
