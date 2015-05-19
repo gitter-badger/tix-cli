c9a44fe522254cef6ec513d6d49ec0b87be866b2
Cole Chamberlain <cole@tix.com>
2015-05-18 18:46:17 -0700

    Updates to tix-cli module...

--- src/sh/install.sh
+++ src/sh/install.sh
@@ -4,13 +4,25 @@
 
 INSTALL_ROOT="${HOME}"
 NPMRC_PATH="${HOME}/.npmrc"
+CSDK_PATH="${INSTALL_ROOT}/src/bin/wdexpress_full.exe"
+NODE_PATH="${LOCAL_BIN_ROOT}/node.exe"
 LOCAL_BIN_ROOT="${INSTALL_ROOT}/local/bin"
 
-echo "--Creating new ~/.npmrc--"
-echo prefix = ${LOCAL_BIN_ROOT:2} > $NPMRC_PATH
+if [ ! -e $NPMRC_PATH ]; then
+  echo "--Creating new ~/.npmrc--"
+  echo prefix = ${LOCAL_BIN_ROOT:2} > $NPMRC_PATH
+fi
 
-echo "--Installing 32-bit node.exe--"
-curl -L http://nodejs.org/dist/v0.12.3/node.exe > "${LOCAL_BIN_ROOT}/node.exe"
+if [ ! -e $CSDK_PATH ]; then
+  echo "--Installing Visual Studio Express Visual C++ SDK"
+  curl -L "http://go.microsoft.com/?linkid=9816758" > $CSDK_PATH
+  cmd.exe $CSDK_PATH
+fi
+
+if [ ! -e $NODE_PATH ]; then
+  echo "--Installing 32-bit node.exe--"
+  curl -L http://nodejs.org/dist/v0.12.3/node.exe > $NODE_PATH
+fi
 
 echo "--Installing NPM--"
 curl -L https://www.npmjs.org/install.sh | sh
