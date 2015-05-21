80dda538bb22f53811330b961c7b24f595116952
cchamberlain <cole.chamberlain@gmail.com>
2015-05-20 22:46:21 -0700

    updated look and feel

--- src/sh/install.sh
+++ src/sh/install.sh
@@ -138,8 +138,8 @@ tix -oxn
 echo "--Full dev clone--"
 clone-all-dev
 
-echo "--Installing custom fonts--"
-install-fonts >> $INSTALL_LOG_PATH
+#echo "--Installing custom fonts--"
+#install-fonts >> $INSTALL_LOG_PATH
 
 echo "--Run JS project in debug mode--"
 debug-js
