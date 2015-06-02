33581e896fa9a0ad9ae9764ed8b956c29f8fd2b0
Cole Chamberlain <cole.chamberlain@gmail.com>
2015-06-02 11:21:08 -0700

    fixed issue that would allow npm / nuget to not install in the portable path

--- src/sh/install.sh
+++ src/sh/install.sh
@@ -12,11 +12,17 @@ SRC_BIN_ROOT="${INSTALL_ROOT}/src/bin"
 CSDK_ROOT="${SRC_BIN_ROOT}/VS2013_4_CE_ENU"
 NUGET_PATH="${LOCAL_BIN_ROOT}/nuget.exe"
 NODE_PATH="${LOCAL_BIN_ROOT}/node.exe"
+NPM_PATH="${LOCAL_BIN_ROOT}/npm.exe"
 RES_CONFIG_PATH="${LOCAL_CONFIG_ROOT}/res.config"
 VS2012_WDX_7Z_PATH="${SRC_BIN_ROOT}/vs2012_wdx.7z"
 VS2012_WDX_INSTALL_ROOT="${SRC_BIN_ROOT}/vs2012_wdx"
 VS2012_WDX_EXE_PATH="${DEVENVDIR}/WDExpress.exe"
 MSVS_VERSION=2012
+
+
+# Ensure paths exist
+mkdir -p "${LOCAL_BIN_ROOT}"
+mkdir -p "${LOCAL_CONFIG_ROOT}"
 
 # Skipping this until tix has a readonly user with private repo access
 #if [ ! -e "$NPMRC_PATH" ]; then
@@ -25,87 +31,40 @@ MSVS_VERSION=2012
 #  echo "//registry.npmjs.org/:_authToken=aca97731-9f24-46fa-a0bb-7f547b937342" >> $NPMRC_PATH
 #fi
 
-
-#if [ ! -f "$VS2012_WDX_EXE_PATH" ]; then
-#  mkdir -p $SRC_BIN_ROOT
-#  pushd $SRC_BIN_ROOT
-#    echo "--Downloading VS2012 express tools for windows desktop...--"
-#    if [ ! -f "$VS2012_WDX_7Z_PATH" ]; then
-#      curl -L https://s3-us-west-2.amazonaws.com/tixinc/vs2012_wdx/vs2012_wdx.7z >vs2012_wdx.7z
-#    fi
-#    if [ ! -d "$VS2012_WDX_INSTALL_ROOT" ]; then
-#      mkdir -p $VS2012_WDX_INSTALL_ROOT
-#      7za x vs2012_wdx.7z -o"$VS2012_WDX"
-#    fi
-#    pushd $VS2012_WDX_INSTALL_ROOT
-#      echo "--Installing VS2012 tools for windows desktop...--"
-#      cmd.exe /c wdexpress_full.exe
-#      echo -n "--Hit enter when finished with install / registration--"
-#      read
-#    popd
-#  popd
-#fi
-
-### choco install of the VS 2012 for windows desktop does not seem to work
-#echo "--Installing Visual Studio Express 2012 for Windows Desktop 11.0--"
-#choco install visualstudio2012wdx --yes --force >> $INSTALL_LOG_PATH
-
-### Optional step for setting up VS 2013 community
-#if [ -d "$CSDK_ROOT" ]; then
-#  echo "--Installing Visual Studio 2013 Community--" | tee $INSTALL_LOG_PATH
-#  echo "--You must install the Visual Studio MFC C++ Tools for this to work--" | tee $INSTALL_LOG_PATH
-#  ### Web installer
-#  ###curl -L "http://go.microsoft.com/?linkid=9816758" > $CSDK_PATH
-#  pushd "${CSDK_ROOT}"
-#  cmd.exe /c vs_community.exe 2>&1 | tee $INSTALL_LOG_PATH
-#  popd
-#  echo -n "Visual studio tools have finished installing.  Register your version of Visual Studio and hit enter..." | tee $INSTALL_LOG_PATH
-#  read
-#fi
-
-### Set window resolution information
+# Set window resolution information
 if hash powershell 2>/dev/null; then
-  echo "--Getting Resolution Info--"
-  mkdir -p $LOCAL_CONFIG_ROOT
+  >&2 echo "--Getting Resolution Info--"
   powershell -NoProfile -Command "Add-Type -AssemblyName System.Windows.Forms;[System.Windows.Forms.Screen]::AllScreens" > $RES_CONFIG_PATH
 fi
 
 # Install node 32-bit portable for development (production gets 64 bit)
-if [ ! -e "$NUGET_PATH" ]; then
-  curl -L "https://nuget.org/nuget.exe" > $NUGET_PATH
+if [ ! -f "$NUGET_PATH" ] ; then
+  >&2 "--Installing Nuget--"
+  curl -L "https://nuget.org/nuget.exe" >"$NUGET_PATH"
 fi
 
-if ! hash node 2>/dev/null; then
-  echo "--Installing 32-bit node.exe--" | tee $INSTALL_LOG_PATH
-  curl -L http://nodejs.org/dist/v0.12.3/node.exe > $NODE_PATH
+if [ ! -f "$NODE_PATH" ] ; then
+  >&2 echo "--Installing 32-bit node.exe--"
+  curl -L http://nodejs.org/dist/v0.12.3/node.exe >"$NODE_PATH"
 fi
 
-### Force UAC popup
-#PowerShell -Command "Start-Process node.exe -Verb RunAs"
-
-if ! hash npm 2>/dev/null; then
-  echo "--Installing NPM--" | tee $INSTALL_LOG_PATH
-  curl -L https://www.npmjs.org/install.sh | sh >> $INSTALL_LOG_PATH
-
-  echo "--Setting VS tools version to ${MSVS_VERSION}" | tee $INSTALL_LOG_PATH
-  npm config set msvs_version ${MSVS_VERSION} --global 2>&1 | tee $INSTALL_LOG_PATH
-  echo "--Setting npm to use bash shell when using npm explore command--"
+if [ ! -f "$NPM_PATH" ] ; then
+  >&2 echo "--Installing NPM--"
+  curl -L https://www.npmjs.org/install.sh | sh >/dev/null
+  >&2 echo "--Setting VS tools version to ${MSVS_VERSION}"
+  npm config set msvs_version "${MSVS_VERSION}" --global
+  >&2 echo "--Setting npm to use bash shell when using npm explore command--"
   npm config set shell bash
-  #echo "--Swapping npm's internal node-gyp for custom one--"
-  #npm explore npm -g -- npm install cchamberlain/node-gyp
 fi
 
-echo "--Installing tix-cli globally--" | tee $INSTALL_LOG_PATH
-npm install -g tix-cli >> $INSTALL_LOG_PATH
+>&2 echo "--Installing tix-cli globally--"
+npm install -g tix-cli >/dev/null
 
-echo "--Running tix-cli with optional applications and extended mode and no-run--" | tee $INSTALL_LOG_PATH
+>&2 echo "--Running tix-cli with optional applications and extended mode and no-run--"
 tix -ioxn
 
-echo "--Full dev clone--"
+>&2 echo "--Full dev clone--"
 clone-all-dev
 
-#echo "--Installing custom fonts--"
-#install-fonts >> $INSTALL_LOG_PATH
-
-echo "--Run JS project in debug mode--"
+>&2 echo "--Run JS project in debug mode--"
 debug-js
